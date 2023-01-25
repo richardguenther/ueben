4c022242e31fc0ef73c8ca32e06ee960983c8534
Richard Günther <richardguenther@fastmail.com>
2023-01-25 16:31:41 +0100

    weiter

--- src/_includes/assets-global/scripts.js
+++ src/_includes/assets-global/scripts.js
@@ -1,4 +1,18 @@
 import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
+import collapse from "@alpinejs/collapse";
+import intersect from "@alpinejs/intersect";
+import smoothscroll from "smoothscroll-polyfill";
+import { createPopper } from "@popperjs/core";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+
+// Alpine.plugin(smoothscroll);
+Alpine.plugin(intersect);
+
+Alpine.plugin(collapse);
+
+Alpine.plugin(focus);
 
 window.Alpine = Alpine;
 
